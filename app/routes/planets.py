ec2df1085c2792d1db94a5887850141702ae1159
Junnie <junniejlee@gmail.com>
2022-04-22 12:52:04 -0700

    worked on wave 01, getting 404 error

---
+++ app/routes/planets.py
@@ -0,0 +1,30 @@
+from flask import Blueprint, jsonify
+
+class Planet:
+    def __init__(self, id, name, description, is_colonized):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.is_colonized = is_colonized
+        
+    def to_dictionary(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "is_colonized": self.is_colonized,    
+        }    
+        
+planets = [
+    Planet(0, "Artemis", "Hot rocky world with lots of metal ores", False),
+    Planet(1, "Calypso", "Shattered remnants of world that once had life", False),
+    Planet(2, "Veridia", "Once a center of civilization, now a runaway greenhouse jungle", True),
+    Planet(3, "Azure Spire", "Capital of The Galactic Imperium", True),
+    Planet(4, "Aura", "Gas giant with 100 moons", True)
+]
+
+planets_bp = Blueprint("planets", __name__, url_prefix = "/planets")
+
+@planets_bp.route("", methods=["GET"])
+def get_all_planets():
+    return jsonify([planet.to_dictionary() for planet in planets])
